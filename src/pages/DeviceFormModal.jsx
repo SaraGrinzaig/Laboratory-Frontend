e99c6e66d2fdc504b7bb9ca4cbcc5907043df822
SaraGrinzaig <saragrinzaig@gmail.com>
2024-09-08 17:02:44 +0300

    save

--- src/pages/DeviceFormModal.jsx
+++ src/pages/DeviceFormModal.jsx
@@ -47,14 +47,23 @@ const DeviceFormModal = () => {
       onRequestClose={closeModal}
       contentLabel="הכנס פרטי מכשיר חדש"
       style={{
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          zIndex: 1000
+        },
         content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-          width: '400px'
+          position: 'absolute',
+          top: '10%',
+          left: '10%',
+          right: '10%',
+          bottom: '10%',
+          border: '1px solid #ccc',
+          background: '#fff',
+          overflow: 'auto',
+          WebkitOverflowScrolling: 'touch',
+          borderRadius: '4px',
+          outline: 'none',
+          padding: '20px'
         }
       }}
     >
